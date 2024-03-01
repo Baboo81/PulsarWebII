f1f99f179b49078c2ca8a543195986cbca9f1c10
Baboo81 <magicbaboo@gmail.com>
2024-03-01 15:07:23 +0100

    created contact page

---
+++ src/views/contact/Contact.jsx
@@ -0,0 +1,12 @@
+import style from './contact.module.css';
+import { motion } from "framer-motion";
+
+const Contact = () => { 
+    return (
+        <section>
+
+        </section>
+    )
+}
+
+export default Contact
