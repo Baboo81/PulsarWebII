e39af5674c0061718fe8ce4b7749e72cd31a080e
Baboo81 <magicbaboo@gmail.com>
2024-03-03 20:39:22 +0100

    homePage

--- src/views/home/HomePage.jsx
+++ src/views/home/HomePage.jsx
@@ -12,21 +12,23 @@ const skills = [
 ]
 
 
-function Title ({color, fontSize}) {
+function Title ({color, fontSize, fontFamily}) {
     return <h1 style = {
                 {
                     color : color,
-                    fontSize : fontSize
+                    fontSize : fontSize,
+                    fontFamily : fontFamily
                 }
                         }> {title}
            </h1>
 }
 
-function Title2 ({color, fontSize}) {
+function Title2 ({color, fontSize, fontFamily}) {
     return <h1 style = {
                 {
                     color : color,
-                    fontSize : fontSize
+                    fontSize : fontSize,
+                    fontFamily : fontFamily
                 }
                        }> {title2}
             </h1>
@@ -37,15 +39,18 @@ const HomePage = () => {
     return (
         <section>
             <div class="container">
-                <div class="row">
-                    <div class="col-md-6">
-                        <motion.p class="p-4 mx-5" initial={{x: 0, opacity: 0}} animate={{x: 100, opacity: 1}}>
+                <div class="row p-3">
+                    <div class="col-md-5 p-5 text-start">
+                        <motion.p class="p-4" initial={{x: 0, opacity: 0}} animate={{x: 100, opacity: 1}}>
                             {skills.map(skill => (<li key={skill}> {skill} </li>))}
                         </motion.p>
                     </div>
-                    <div class="col-md-6">
-                        <Title color="#6486ac" fontSize="16em" />
-                        <Title2 color="#6486ac" fontSize="10em"/>
+                    <div className="col-md-3">
+
+                    </div>
+                    <div class="col-md-3">
+                        <Title color="#6486ac" fontSize="16em" fontFamily="Nanum Myeongjo" />
+                        <Title2 color="#6486ac" fontSize="10em" fontFamily="Nanum Myeongjo" />
                     </div>
                 </div>
             </div>
