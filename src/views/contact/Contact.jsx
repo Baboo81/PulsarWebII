36e501fe9766b319f54b27b6b5aed4e2d6b7a744
Baboo81 <magicbaboo@gmail.com>
2024-03-22 11:26:13 +0100

    contact

--- src/views/contact/Contact.jsx
+++ src/views/contact/Contact.jsx
@@ -7,9 +7,10 @@ const Contact = () => { 
         <section>
             <div className="container">
                 <div className="row">
-                    <div className="col-md-12">
-                        <article>
-                            
+                    <div className="col-md-12 p-5">
+                        <article className="p-3">
+                            <h1>Adresse</h1>
+                            <p>Rue champ rodange 14, 1410 Waterloo</p>
                         </article>
                     </div>
                 </div>
