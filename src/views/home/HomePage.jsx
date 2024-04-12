d67467cf988b7bf9d5eab5190a9bec127b9ef52e
Baboo81 <magicbaboo@gmail.com>
2024-04-12 16:30:01 +0200

    HomePage

--- src/views/home/HomePage.jsx
+++ src/views/home/HomePage.jsx
@@ -40,17 +40,17 @@ const HomePage = () => {
         <section>
             <div class="container">
                 <div class="row p-3">
-                    <div class="col-md-5 p-5 text-start">
-                        <motion.p class="p-4" initial={{x: 0, opacity: 0}} animate={{x: 100, opacity: 1}}>
-                            {skills.map(skill => (<li key={skill}> {skill} </li>))}
-                        </motion.p>
+                    <div class="col-md-3 p-3">
+                        <Title color="#6486ac" fontSize="16em" fontFamily="Nanum Myeongjo" />
+                        <Title2 color="#6486ac" fontSize="16em" fontFamily="Nanum Myeongjo" />
                     </div>
                     <div className="col-md-3">
 
                     </div>
-                    <div class="col-md-3 p-3">
-                        <Title color="#6486ac" fontSize="16em" fontFamily="Nanum Myeongjo" />
-                        <Title2 color="#6486ac" fontSize="16em" fontFamily="Nanum Myeongjo" />
+                    <div class="col-md-5 p-5 text-start">
+                        <motion.p class="p-4" initial={{x: 0, opacity: 0}} animate={{x: 100, opacity: 1}}>
+                            {skills.map(skill => (<li key={skill}> {skill} </li>))}
+                        </motion.p>
                     </div>
                 </div>
             </div>
