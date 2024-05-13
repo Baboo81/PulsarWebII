fc6f3e4c0c215e3b5c5ff6aa091aaa99cfcb59a0
Baboo81 <magicbaboo@gmail.com>
2024-05-13 10:01:17 +0200

    footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,9 +4,11 @@ const Footer = () => (
 
     <footer className="container">
         <div className="row">
-            <a href="https://github.com/Baboo81"><img className={style.pictos} src="../../public/svg/footer/PictoGit.svg" alt="Icone GitHub" /></a>
-            <a href="https://www.linkedin.com/in/chris-rodriguez-perez-609716100/"><img className={style.pictos} src="../../public/svg/footer/PictoLinkedIn.svg" alt="Icone LinkedIn" /></a>
-            <a href="mailto:chrisrodirguez@hotmail.be"><img className={style.pictos} src="../../public/svg/footer/PictoMail.svg" alt="Icone Email" /></a>
+            <div className="col-md-12">
+                <a href="https://github.com/Baboo81"><img className={style.pictos} src="../../public/svg/footer/PictoGit.svg" alt="Icone GitHub" /></a>
+                <a href="https://www.linkedin.com/in/chris-rodriguez-perez-609716100/"><img className={style.pictos} src="../../public/svg/footer/PictoLinkedIn.svg" alt="Icone LinkedIn" /></a>
+                <a href="mailto:chrisrodirguez@hotmail.be"><img className={style.pictos} src="../../public/svg/footer/PictoMail.svg" alt="Icone Email" /></a>
+            </div>
         </div>
     </footer>
 
