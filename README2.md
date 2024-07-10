044c2252d8f8d417a23b8c15bfd43e6807b80b97
Baboo81 <magicbaboo@gmail.com>
2024-07-10 14:55:51 +0200

    Added txt, logo

--- README2.md
+++ README2.md
@@ -1,6 +1,7 @@
-#Liste : Alexis Poulain, Senta Depuydt, Denis Rancourt, Christian Gerondeau,   Karlzero, Pierre Barnerias, Youssef Hindi.
+#Liste : Alexis Poulain, Senta Depuydt, Denis Rancourt, Christian Gerondeau, Karlzero, Pierre Barnerias, Youssef Hindi, Silvano Trotta.
 #Sécuriser le site.
 #Contactez les protagonistes via Linkedin.
 #Création d'un logo.
 #Refaire les vignettes non-conformes.
 #Création d'un espace commentaire.
+#Explorer React Native.
--- src/views/home/HomePage.jsx
+++ src/views/home/HomePage.jsx
@@ -70,7 +70,7 @@ const HomePage = () => {
                 </div>
                 <div className="row">
                     <MainTitle title="Une information de qualité" />
-                    <motion.p className="p-5">Information Liber a pour objectif d'apporter un nouvel angle de vue sur des thématiques qui sont de nos jours manipulées par les médias de masse. La sélèction des différentes plate-formes est basée sur des critères tels que ; l'honnêteté intéllectuelle mais aussi sur l'intégrite de ces auteurs. Á l'heure actuelle les médias de grand chemin ne jouissent plus de l'indépendance dont ils devraient être garants, d'où l'intérrêt d'avoir mis sur pied ce site qui vous permettra de découvrir une série de plate-forme qui oeuvre dans le sens de la souverainté intelectuelle, le libre arbitre.
+                    <motion.p className="p-5 text-muted">Info Liber a pour objectif d'apporter un nouvel angle de vue sur des thématiques qui sont de nos jours manipulées par les médias de masse. La sélèction des différentes plate-formes est basée sur des critères tels que ; l'honnêteté intéllectuelle mais aussi sur l'intégrite de ces auteurs. Á l'heure actuelle les médias de grand chemin ne jouissent plus de l'indépendance dont ils devraient être garants, d'où l'intérrêt d'avoir mis sur pied ce site qui vous permettra de découvrir une série de plate-forme qui oeuvre dans le sens de la souverainté intelectuelle, le libre arbitre.
                     </motion.p>
                 </div>
             </div>
