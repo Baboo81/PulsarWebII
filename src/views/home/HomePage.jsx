c437e5120a86b9b768af62d39e32e3d98502fb0d
Baboo81 <magicbaboo@gmail.com>
2024-07-11 10:36:59 +0200

    HomePage.jsx

--- src/views/home/HomePage.jsx
+++ src/views/home/HomePage.jsx
@@ -70,15 +70,11 @@ const HomePage = () => {
                 <div className="row">
                     <MainTitle title="Une information de qualité" />
                     <div className="justify-content-left p-4">
-                        <div>
-                            <img src="public/svg/guillemetsO.svg" alt="dessin de guillemets ouvrants" />
-                        </div>
-                        <motion.p className="p-5 text-muted">Info Liber a pour objectif d'apporter un nouvel angle de vue sur des thématiques qui sont de nos jours manipulées par les médias de masse. La sélèction des différentes plate-formes est basée sur des critères tels que ; l'honnêteté intéllectuelle mais aussi sur l'intégrité de ces auteurs. Á l'heure actuelle les médias de grand chemin ne jouissent plus de l'indépendance dont ils devraient être garants, d'où l'intérrêt d'avoir mis sur pied ce site qui vous permettra de découvrir une série de plate-forme qui oeuvre dans le sens de la souverainté intelectuelle, le libre arbitre.
+                        <div className={style.guillemetsO}></div>
+                        <motion.p className="text-muted">Info Liber a pour objectif d'apporter un nouvel angle de vue sur des thématiques qui sont de nos jours manipulées par les médias de masse. La sélèction des différentes plate-formes est basée sur des critères tels que ; l'honnêteté intéllectuelle mais aussi sur l'intégrité de ces auteurs. Á l'heure actuelle les médias de grand chemin ne jouissent plus de l'indépendance dont ils devraient être garants, d'où l'intérrêt d'avoir mis sur pied ce site qui vous permettra de découvrir une série de plate-forme qui oeuvre dans le sens de la souverainté intelectuelle, le libre arbitre.
                         </motion.p>
-                        <div>
-                            <img src="public/svg/guillemetsF.svg" alt="dessin de guillemets fermants" />
-                        </div>
-                        <p className="text-muted p-4">-- Fruit des recherches d'une chercheuse de vérité --</p>
+                        <div className={style.guillemetsF}></div>
+                        <p className="text-muted">-- <i>Fruit des recherches d'une chercheuse de vérité</i> --</p>
                     </div>
                 </div>
             </div>
