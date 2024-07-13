b5077a34e41d6dc28b78af2e6b58161f1561997d
Baboo81 <magicbaboo@gmail.com>
2024-07-13 17:20:38 +0200

    Added card

--- src/views/geopolitique/Geopolitique.jsx
+++ src/views/geopolitique/Geopolitique.jsx
@@ -17,14 +17,13 @@ const Geopolitique = () => {
                         <MainTitle title= "La géopolitique" />
                     </div>
                     <div className="col-md-4 p-4">
-                        <article className={style.art1}>
-                           
-                        </article>
-                        <div>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda cupiditate officiis mollitia earum ullam voluptate aut odio nam, velit labore nulla numquam in voluptas consequuntur voluptatum facere qui maiores error!</p>
-                        </div>
-                        <div className="p-3">
-                                <Btn url="https://scanderia.com/"/>
+                        <div class="card">
+                            <img src="..." class="card-img-top" alt="..." />
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                            </div>
                         </div>
                     </div>
                 </div>
