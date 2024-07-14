82d8ea634f44ebb43fb87c03de3c40248a4558f1
Baboo81 <magicbaboo@gmail.com>
2024-07-14 17:34:51 +0200

    Added cards

--- src/views/sante/Sante.jsx
+++ src/views/sante/Sante.jsx
@@ -2,6 +2,11 @@ import Btn from '../../components/Btn/Btn';
 import style from './sante.module.css';
 import { motion } from "framer-motion";
 import MainTitle from '../../components/MainTitle/MainTitle';
+import EmaKrusi from './EmaKrusi.svg';
+import IreneGrosjean from './IreneGrosjean.svg';
+import JjCrevecoeur from './JjCrevecoeur.svg';
+import MarieSophieL from './MarieSophieL.svg';
+import ThierryCasasnovas from './ThierryCasasnovas.svg';
 
 
 
@@ -14,74 +19,61 @@ const Sante = () => { 
                         <MainTitle title= "La santé" />
                     </div>
                     <div className="col-md-4 p-4">
-                        <article className={style.art1}>
-                            
-                        </article>
-                        <div>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda cupiditate officiis mollitia earum ullam voluptate aut odio nam, velit labore nulla numquam in voluptas consequuntur voluptatum facere qui maiores error!</p>
-                        </div>
-                        <div className="p-3">
-                            <Btn />
+                        <div class="card">
+                            <img src={EmaKrusi} class="card-img-top" alt="..." />
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="https://emakrusi.com/" class="btn btn-primary">Go somewhere</a>
+                            </div>
                         </div>
                     </div>
                     <div className="col-md-4 p-4">
-                        <article className={style.art2}>
-                            
-                        </article>
-                        <div>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda cupiditate officiis mollitia earum ullam voluptate aut odio nam, velit labore nulla numquam in voluptas consequuntur voluptatum facere qui maiores error!</p>
-                        </div>
-                        <div className="p-3">
-                            <Btn />
+                        <div class="card">
+                            <img src={IreneGrosjean} class="card-img-top" alt="..." />
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="https://www.youtube.com/channel/UCX6iYvJWGOQfGsFo2KSSm-w" class="btn btn-primary">Go somewhere</a>
+                            </div>
                         </div>
                     </div>
                 </div>
                 <div className="row justify-content-center">
                     <div className="col-md-4 p-4">
-                        <article className={style.art3}>
-
-                        </article>
-                        <div>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda cupiditate officiis mollitia earum ullam voluptate aut odio nam, velit labore nulla numquam in voluptas consequuntur voluptatum facere qui maiores error!</p>
-                        </div>
-                        <div className="p-3">
-                            <Btn />
+                        <div class="card">
+                            <img src={JjCrevecoeur} class="card-img-top" alt="..." />
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="https://formations.emergences.net/login" class="btn btn-primary">Go somewhere</a>
+                            </div>
                         </div>
                     </div>
                     <div className="col-md-4 p-4">
-                        <article className={style.art4}>
-
-                        </article>
-                        <div>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda cupiditate officiis mollitia earum ullam voluptate aut odio nam, velit labore nulla numquam in voluptas consequuntur voluptatum facere qui maiores error!</p>
-                        </div>
-                        <div className="p-3">
-                            <Btn />
+                        <div class="card">
+                            <img src={MarieSophieL} class="card-img-top" alt="..." />
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="https://mariesophiel.com/" class="btn btn-primary">Go somewhere</a>
+                            </div>
                         </div>
                     </div>
                 </div>
                 <div className="row justify-content-center">
                     <div className="col-md-4 p-4">
-                        <article className={style.art5}>
-
-                        </article>
-                        <div>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda cupiditate officiis mollitia earum ullam voluptate aut odio nam, velit labore nulla numquam in voluptas consequuntur voluptatum facere qui maiores error!</p>
-                        </div>
-                        <div className="p-3">
-                            <Btn />
+                        <div class="card">
+                            <img src={ThierryCasasnovas} class="card-img-top" alt="..." />
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="https://www.rgnr.tv/" class="btn btn-primary">Go somewhere</a>
+                            </div>
                         </div>
                     </div>
                     <div className="col-md-4 p-4">
-                        <article className={style.art6}>
-
-                        </article>
-                        <div>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda cupiditate officiis mollitia earum ullam voluptate aut odio nam, velit labore nulla numquam in voluptas consequuntur voluptatum facere qui maiores error!</p>
-                        </div>
-                        <div className="p-3">
-                            <Btn />
-                        </div>
+                        
                     </div>
                 </div>
             </div>
