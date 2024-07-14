21658a5c9cbe804abeb3f78393559a12b9e82327
Baboo81 <magicbaboo@gmail.com>
2024-07-14 12:07:13 +0200

    Modified geopolitic's structure

--- src/views/geopolitique/Geopolitique.jsx
+++ src/views/geopolitique/Geopolitique.jsx
@@ -6,6 +6,9 @@ import Slider from '../../components/Slider/Slider';
 import TitleMain from '../../components/MainTitle/MainTitle';
 import Btn from '../../components/Btn/Btn';
 import MainTitle from '../../components/MainTitle/MainTitle';
+import IdrissAberkane from './IdrissAberkane.svg';
+import PierreHillard from './PierreHillard.svg';
+import ThierryMeyssan from './ThierryMeyssan.svg';
 
 
 const Geopolitique = () => {
@@ -18,41 +21,39 @@ const Geopolitique = () => {
                     </div>
                     <div className="col-md-4 p-4">
                         <div class="card">
-                            <img src="..." class="card-img-top" alt="..." />
+                            <img src={IdrissAberkane} class="card-img-top" alt="..." />
                             <div class="card-body">
                                 <h5 class="card-title">Card title</h5>
                                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                                <a href="https://scanderia.com/" class="btn btn-primary">Go somewhere</a>
                             </div>
                         </div>
                     </div>
                 </div>
                 <div className="row justify-content-center">
                     <div className="col-md-4 p-4">
-                        <article className={style.art2}>
-
-                        </article>
-                        <div>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda cupiditate officiis mollitia earum ullam voluptate aut odio nam, velit labore nulla numquam in voluptas consequuntur voluptatum facere qui maiores error!</p>
-                        </div>
-                        <div className="p-3">
-                                <Btn src=""/>
+                        <div class="card">
+                            <img src={ThierryMeyssan} class="card-img-top" alt="..." />
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="https://scanderia.com/" class="btn btn-primary">Go somewhere</a>
+                            </div>
                         </div>
                     </div>
                 </div>
                 <div className="row justify-content-center">
                     <div className="col-md-4 p-4">
-                            <article className={style.art3}>
-
-                            </article>
-                            <div>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda cupiditate officiis mollitia earum ullam voluptate aut odio nam, velit labore nulla numquam in voluptas consequuntur voluptatum facere qui maiores error!</p>
-                        </div>
-                            <div className="p-3">
-                                <Btn />
-                        </div>
+                        <div class="card">
+                            <img src={PierreHillard} class="card-img-top" alt="..." />
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="https://scanderia.com/" class="btn btn-primary">Go somewhere</a>
+                            </div>
                         </div>
                     </div>
+                </div>
             </div>
         </section>
     )
