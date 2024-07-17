217dc4c4b46d542071380947ec26183894fe4ae6
Baboo81 <magicbaboo@gmail.com>
2024-07-17 12:05:01 +0200

    Added cards

--- src/views/actualites/Actualite.jsx
+++ src/views/actualites/Actualite.jsx
@@ -4,6 +4,8 @@ import { motion } from "framer-motion";
 import MainTitle from '../../components/MainTitle/MainTitle';
 import AlexandrePenasse from './AlexandrePenasse.svg';
 import SalimLaïbi from './SalimLaïbi.svg';
+import JDMichel from './JDMichel.svg';
+import PierreBarnerias from './PierreBarnerias.svg';
 
 
 
@@ -32,6 +34,28 @@ const Actualite = () => { 
                                 <h5 class="card-title">Card title</h5>
                                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                                 <a href="https://www.lelibrepenseur.org/" class="btn btn-primary">Aller vers ce site</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="row justify-content-center">
+                    <div className="col-md-4 p-4">
+                        <div class="card">
+                            <img src={JDMichel} class="card-img-top" alt="..." />
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="https://www.jdmichel.tv/" class="btn btn-primary">Aller vers ce site</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-4 p-4">
+                        <div class="card">
+                            <img src={PierreBarnerias} class="card-img-top" alt="..." />
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="https://citizen-light.fr/actualites/" class="btn btn-primary">Aller vers ce site</a>
                             </div>
                         </div>
                     </div>
