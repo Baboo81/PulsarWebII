2f82f33ad8a2fe54aa33af1fb5d9da6ba959cda7
Baboo81 <magicbaboo@gmail.com>
2024-07-18 10:27:47 +0200

    Modifed Contact.jsx

--- src/components/Btn/Btn.jsx
+++ src/components/Btn/Btn.jsx
@@ -1,10 +1,10 @@
 import style from './btn.module.css';
 
 const Btn = function(props) {
-    const  {url} = props;
+    const  {title} = props;
 
     return (
-        <button>Aller vers ce site<a href={url}></a></button>
+        <button>{title}</button>
     );
     
 }
--- src/views/Contact/Contact.jsx
+++ src/views/Contact/Contact.jsx
@@ -3,6 +3,7 @@ import NavBar from '../../components/NavBar/NavBar';
 import { Fragment } from 'react';
 import { motion } from 'framer-motion';
 import MainTitle from '../../components/MainTitle/MainTitle';
+import Btn from '../../components/Btn/Btn';
 
 
 const Contact = () => {
@@ -12,7 +13,12 @@ const Contact = () => {
                 <div className="row">
                     <MainTitle title="Laissez un commentaire" /> 
                     <div className="col-md-12">
-                        
+                        <div className="p-5">
+                            <textarea name="" id="textarea" rows={10} cols={60}></textarea>
+                        </div>
+                        <div className="p-3">
+                            <Btn title="Envoyer" />
+                        </div>
                     </div>
                 </div>
             </div>
