de422736664bd42b7462f311e422a952531eb800
Baboo81 <magicbaboo@gmail.com>
2024-07-22 14:32:43 +0200

    Added content Idriss.A

--- src/views/geopolitique/Geopolitique.jsx
+++ src/views/geopolitique/Geopolitique.jsx
@@ -22,10 +22,25 @@ const Geopolitique = () => {
                     <div className="col-md-4 p-4">
                         <div class= {style.card} >
                             <img src={IdrissAberkane} class="card-img-top" alt="..." />
-                            <div class="card-body">
-                                <h5 class="card-title mb-3">Idriss Aberkane</h5>
-                                <p class="card-text text-muted">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                <a href="https://scanderia.com/" class="btn btn-outline-danger">Aller vers ce site</a>
+                            <div class="card-body p-2">
+                                <h5 class="card-title mb-3 p-3">Écrivain, conférencier & chercheur</h5>
+                                <p class="card-text text-muted p-2">Idriss.A possède trois doctorats dans les domaines suivants: 
+                                    <ol class="list-group list-group-numbered">
+                                        <li class="list-group-item">Littérature Comparée</li>
+                                        <li class="list-group-item">Diplomatie & Relations Internationales</li>
+                                        <li class="list-group-item">Neurosciences</li>
+                                    </ol>
+                                Pour visiter son site personnel cliquer sur le bouton ci-dessous:
+                                </p>
+                                <div className="p-2">
+                                    <a href="https://idrissaberkane.org/" class="btn btn-outline-danger">Site personnel Idriss.A</a>
+                                </div>
+                                <p class="card-text text-muted p-2">
+                                    Idriss.A s'intérresse à une multitude de sujets autre que la géopolitique tels que les sciences, l'économie, l'I.A, ... Il développe ces thématiques avec beaucoup de rigueur et de neutralité. Idriss.A a crée Scanderia qui est un site axé sur l'apprentissage de divers sujets; économie, géopolitique, ...
+                                </p>
+                                <div>
+                                    <a href="https://scanderia.com/" class="btn btn-outline-danger">Aller vers le site : Scanderia</a>
+                                </div>
                             </div>
                         </div>
                     </div>
