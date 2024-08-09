f19fc62f67707efa7db5730ec282dc1f98926c7c
Baboo81 <magicbaboo@gmail.com>
2024-08-09 10:27:57 +0200

    Slider settings

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -3,10 +3,11 @@ import { motion } from "framer-motion";
 import CarouselSlider from 'react-carousel-slider';
 
 
+
 let data = [
   {
     desc: "1",
-    imgSrc: "./public/img/livres/IdrissA/Livre1.png"
+    imgSrc: 'Livre1.png'
   },
   {
     desc: "2",
@@ -18,7 +19,7 @@ let data = [
   },
   {
     desc: "4",
-    imgSrc: ""
+    imgSrc: "./public/img/livres/IdrissA/Livre2.png"
   },
   {
     desc: "5",
@@ -32,37 +33,37 @@ let sliderBoxStyle = {
 };
 
 let itemsStyle = {
-  // ,height: "100%", padding: "0px"
-  // , padding: "15px"
-  // , background: "#FFCA28"
-  // , borderRadius: "4px"
-  // , margin: "0px 0px", padding: "0px"
+     height: "100%", padding: "0px"
+   , padding: "15px"
+   , background: "#050000"
+   , borderRadius: "4px"
+   , margin: "0px 0px", padding: "0px"
 };
 
 let textBoxStyle = {
-  // textAlign: "left"
-  // ,width:"50%"
-  // , background: "transparent"
-  // , fontSize: "36px"
-  // , fontWeight: 300
+    textAlign: "left"
+    ,width:"50%"
+    , background: "transparent"
+    , fontSize: "36px"
+    , fontWeight: 300
 };
 
 let buttonSetting = {
-  // placeOn: "middle-inside"
-  // ,hoverEvent: true,
-  // , style: {
-  //   left: {
-  //     margin: "0px 0px 0px 10px"
-  //   },
-  //   right: {
-  //     margin: "0px 10px 0px 0px"
-  //   }
-  // }
+   placeOn: "middle-inside"
+   ,hoverEvent: true,
+    style: {
+              left: {
+              margin: "0px 0px 0px 10px"
+              },
+              right: {
+              margin: "0px 10px 0px 0px"
+              }
+            }
 };
 
 let manner = {
-  // autoSliding: {interval: "4s"}
-  //, duration: "0.3s"
+   autoSliding: {interval: "4s"}
+  , duration: "0.3s"
 };
 
 const Slider = () => { 
