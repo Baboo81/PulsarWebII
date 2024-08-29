f24c93a432a5cb2fe89e709bbc0bc688d1c19b54
Baboo81 <magicbaboo@gmail.com>
2024-08-29 09:48:36 +0200

    SearchBar

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -3,36 +3,32 @@ import { useState } from "react";
 
 const SearchBar = () => {
 
-    const dataObjects = [
-        { theme: "geopolitique", author: "Idriss Aberkane"},
-        { theme: "geopolitique", author: "Thyerry Meyssan"},
-        { theme: "geopolitique", author: "Pierre Hillard"},
-        { theme: "economie", author: "Pierre Chaillot", "Pierre Jovanovic"},
+    const dataArray = [
+        "Géopolitique",
+        "Économie",
+        "Souveraineté",
+        "Santé",
+        "Actualité",
+        "Livres",
     ];
 
-    const [value, setValue] = useState("");
+    const dataObjects = [
+        { theme: "Géopolitique", author: "Idriss Aberkane"},
+        { theme: "Géopolitique", author: "Thyerry Meyssan"},
+        { theme: "Géopolitique", author: "Pierre Hillard"},
+        { theme: "Économie", author: "Pierre Chaillot", "Pierre Jovanovic"},
+    ];
+
+    const [data, setData] = useState(dataArray);
+    const [data2, setData2] = useState(dataObjects);
 
     function handleChange (event) {
         setValue(event.target.value)
     }
 
     return (
-        <div className={style.searchBar}>
-            <div className={style.inputSearch}>
-                <input type="text" value={value} onChange={handleChange} />
-                <button>
-                    <span className="material-symbols-outlined">
-                        Search
-                    </span>
-                </button>
-            </div>
-            <ul>
-                {value && 
-                    data.filter((element) => element.includes(value))
-                        .map((element, index) => <li onClick={() => setValue(element)} key={index}> {element} </li>)
-                }
-            </ul>
-        </div>
+      <>
+      </>
     )
 
 }
