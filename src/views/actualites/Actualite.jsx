75f8615fb87fef1ad9fc809644d647ace4e0ce5c
Baboo81 <magicbaboo@gmail.com>
2024-09-11 09:14:45 +0200

    Actualité.jsx

--- src/views/actualites/Actualite.jsx
+++ src/views/actualites/Actualite.jsx
@@ -15,12 +15,12 @@ const Actualite = () => { 
         <section>
             <div className="container">
                 <div className="row justify-content-center">
-                    <div className="p-5">
+                    <div className="p-5 mt-5">
                         <MainTitle title= "Actualités" className="box" id="box1" />
                     </div>
-                    <div className="col-md-4 p-4">
-                        <div class="card">
-                            <img src={AlexandrePenasse} class="card-img-top" alt="..." />
+                    <div className="col-md-5 p-4">
+                        <div class={style.card}>
+                            <img src={AlexandrePenasse} class="card-img-top rounded" alt="Image représentant des journalistes en action" />
                             <div class="card-body">
                                 <h5 class="card-title">Card title</h5>
                                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
