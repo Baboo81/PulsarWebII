a2c41df9628f23c144dad32c66536e092a47e217
Baboo81 <magicbaboo@gmail.com>
2024-09-12 13:47:57 +0200

    Actualité.jsx

--- src/views/actualites/Actualite.jsx
+++ src/views/actualites/Actualite.jsx
@@ -44,7 +44,7 @@ const Actualite = () => { 
                         <div class="card">
                             <img src={JDMichel} class="card-img-top" alt="..." />
                             <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
+                                <h5 class="card-title">Anthropologue & expert en santé</h5>
                                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                                 <a href="https://www.jdmichel.tv/" class="btn btn-primary">Aller vers ce site</a>
                             </div>
