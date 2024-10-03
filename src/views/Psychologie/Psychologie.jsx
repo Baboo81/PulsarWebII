cfba4f34bbf3d86c461ff7ee59f4a95790dc4eba
Baboo81 <magicbaboo@gmail.com>
2024-10-03 15:03:07 +0200

    Psychologie.jsx

--- src/views/Psychologie/Psychologie.jsx
+++ src/views/Psychologie/Psychologie.jsx
@@ -14,14 +14,15 @@ const Psychologie = () => {
                     <div class={style.card}>
                             <img src={ArianeBilheran} class="card-img-top rounded" alt="Image représentant des déssins sur du sable" />
                             <div class="card-body p-2">
-                                <h5 class="card-title mb-3 p-3">Psychopraticienne & formatrice</h5>
-                                <p class="card-text text-muted p-2">Ema est connue pour ses travaux sur la naissance, le couple ainsi que ses prises de positions publiques sur l'autonomie, l'esprit critique, la manipulation de l'information et la psychologie.</p>
+                                <h5 class="card-title mb-3 p-3">Normalienne, philosophe, psychologue clinicienne & docteur en psychopathologie</h5>
+                                <p class="card-text text-muted p-2">Ariane est l'auteur de plusieurs livres, dont les références seront reprisent sur la page livres.<br />Ariane anime des conférences, elle est experte en psychopathologie du pouvoir. Ariane décrit avec beaucoup de précision les mécanismes du totalitarisme, son livre intitulé: "psychopathologie du totalitarisme" est une merveille à lire.</p>
                                 <div className="d-grid gap-2 p-2">
-                                    <a href="https://classic.emakrusi.com/" class="btn btn-outline-primary">Pour visiter le site de Ema</a>
+                                    <a href="https://www.arianebilheran.com/" class="btn btn-outline-primary">Pour visiter le site de Ariane</a>
                                 </div>
-                                <p class="card-text text-muted p-2">Ema s'est spécialisée dans la périnatalité en proposant un programme d'accompagnement aux futurs parents. Dans ce cadre là, Ema à lancer un site spécialement dédié à cette thématique.</p>
                                 <div className="d-grid gap-2 p-2">
-                                    <a href="https://emakrusi.com/" class="btn btn-outline-primary">Site Mastermind de la naissance</a>
+                                         <a href="https://x.com/arianebilheran?lang=fr" class="btn btn-outline-primary">Pour accéder au site<svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-twitter-x mx-3" viewBox="0 0 16 16">
+                                            <path d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865z"/>
+                                    </svg></a>
                                 </div>
                             </div>
                         </div>
